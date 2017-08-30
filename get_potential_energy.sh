32fafb9f223c8b862161b84ca3a3a223dbc62408
bcntls2016 <francois.coppens@irsamc.ups-tlse.fr>
2017-08-30 14:27:35 +0200

    Modified central script

--- get_potential_energy.sh
+++ get_potential_energy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+INPUT=${1}
+ln -sf ${INPUT} djogger.dat
 ./4hetddft-anisotropic < input > res.dat
 TIME=$(head -n 3 djogger.dat | awk '/Total evolution time/{print$5}')
 EINT=$(awk '/Interaction energy/{print$5}' res.dat)
+rm djogger.dat
 echo ${TIME} ${EINT}
