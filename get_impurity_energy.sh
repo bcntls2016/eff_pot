9bc7180cc5d2063751bb57124f3ff3ea47435ecd
bcntls2016 <francois.coppens@irsamc.ups-tlse.fr>
2017-08-30 16:17:00 +0200

    fddf

--- get_impurity_energy.sh
+++ get_impurity_energy.sh
@@ -6,8 +6,11 @@ INPUT=${1}
 ln -sf ${INPUT} djogger.dat
 ./4hetddft-anisotropic < input > res.dat
 TIME=$(head -n 3 djogger.dat | awk '/Total evolution time/{print$5}')
+XIMP=$(head djogger.dat | awk 'NR==9{print$1}')
+YIMP=$(head djogger.dat | awk 'NR==9{print$2}')
+ZIMP=$(head djogger.dat | awk 'NR==9{print$3}')
 EKIN=$(awk '/Kinetic energy \(X/{print$5}' res.dat)
 EINT=$(awk '/Interaction energy/{print$5}' res.dat)
 ESO=$(awk '/Spin-Orbit energy/{print$5}' res.dat)
 rm djogger.dat
-printf "%.1f\t%f\t%f\t%f\n" ${TIME} ${EKIN} ${EINT} ${ESO}
+printf "%.1f\t%f\t%f\t%f\t%f\t%f\t%f\n" ${TIME} ${XIMP} ${YIMP} ${ZIMP} ${EKIN} ${EINT} ${ESO}
