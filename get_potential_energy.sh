00ca671646fecd96e4dd245669deddec69a88ea3
bcntls2016 <francois.coppens@irsamc.ups-tlse.fr>
2017-08-30 15:22:59 +0200

    dsdsadd

--- get_potential_energy.sh
+++ get_potential_energy.sh
@@ -5,5 +5,6 @@ ln -sf ${INPUT} djogger.dat
 ./4hetddft-anisotropic < input > res.dat
 TIME=$(head -n 3 djogger.dat | awk '/Total evolution time/{print$5}')
 EINT=$(awk '/Interaction energy/{print$5}' res.dat)
+ESO=$(awk '/Spin-Orbit energy/{print$5}' res.dat)
 rm djogger.dat
-echo ${TIME} ${EINT}
+echo ${TIME} ${EINT} ${ESO}
