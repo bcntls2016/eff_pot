c0ae6889ccf1e0ba9c11adc80e80dd9623af6ffa
bcntls2016 <francois.coppens@irsamc.ups-tlse.fr>
2017-08-30 15:51:31 +0200

    fddf

--- get_impurity_energy.sh
+++ get_impurity_energy.sh
@@ -3,7 +3,7 @@
 ### CALL: ./get_impurity_energy.sh PATH/TO/WAVE/FUNCTION/FILE
 
 INPUT=${1}
-ln -sf ${INPUT} djogger.dat
+ln -svf ${INPUT} djogger.dat
 ./4hetddft-anisotropic < input > res.dat
 TIME=$(head -n 3 djogger.dat | awk '/Total evolution time/{print$5}')
 EKIN=$(awk '/Kinetic energy \(X/{print$5}' res.dat)
