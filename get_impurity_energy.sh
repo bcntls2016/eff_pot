c38b7ba101d1cd35126e89e18eecf797f9dc6b3e
bcntls2016 <francois.coppens@irsamc.ups-tlse.fr>
2017-08-30 15:37:27 +0200

    Changes to get_impurity_energy.sh

--- get_impurity_energy.sh
+++ get_impurity_energy.sh
@@ -8,5 +8,4 @@ EKIN=$(awk '/Kinetic energy \(X/{print$5}' res.dat)
 EINT=$(awk '/Interaction energy/{print$5}' res.dat)
 ESO=$(awk '/Spin-Orbit energy/{print$5}' res.dat)
 rm djogger.dat
-echo ${TIME} ${EKIN} ${EINT} ${ESO}
-printf "%f\t%f\t%f\t%f" ${TIME} ${EKIN} ${EINT} ${ESO}
+printf "%.1f\t%f\t%f\t%f" ${TIME} ${EKIN} ${EINT} ${ESO}
