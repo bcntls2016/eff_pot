22ef672749e44ad1fed16180cef8ab4cd15d9f9a
bcntls2016 <francois.coppens@irsamc.ups-tlse.fr>
2017-08-30 15:27:41 +0200

    Changes

---
+++ get_impurity_energy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+INPUT=${1}
+ln -sf ${INPUT} djogger.dat
+./4hetddft-anisotropic < input > res.dat
+TIME=$(head -n 3 djogger.dat | awk '/Total evolution time/{print$5}')
+EKIN=$(awk '/Kinetic energy \(X/{print$5}' res.dat)
+EINT=$(awk '/Interaction energy/{print$5}' res.dat)
+ESO=$(awk '/Spin-Orbit energy/{print$5}' res.dat)
+rm djogger.dat
+echo ${TIME} ${EKIN} ${EINT} ${ESO}
