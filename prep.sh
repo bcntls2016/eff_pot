46c8d58be1099272cb8675a993fd7dc0b52672aa
François Coppens <francois.coppens@irsamc.ups-tlse.fr>
2017-09-19 15:33:20 +0200

    changes

--- prep.sh
+++ prep.sh
@@ -7,8 +7,8 @@ R_CUTOFF_PLUS=2.5d0
 UMAX_PLUS=3.5d3
 
 cp -v ${INPUT} input.dat
-sed -i "/filedenin/c\ filedenin = 'djogger.dat'" input.dat
-sed -i "/mode/c\ mode = 3" input.dat
+sed -i "/filedenin/c\ filedenin\t\t= 'djogger.dat'" input.dat
+sed -i "/mode/c\ mode\t\t= 3" input.dat
 sed -i "/\&input/a\ selec_plus\t\t= '${X_PLUS}'\n \
 r_cutoff_plus\t\t= ${R_CUTOFF_PLUS}\n \
 umax_plus\t\t= ${UMAX_PLUS}" input.dat
