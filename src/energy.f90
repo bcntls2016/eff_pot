ab6e20e313ce004c1aae7d93e2a6930ae4873e03
François Coppens <francois.coppens@irsamc.ups-tlse.fr>
2017-09-20 15:29:13 +0200

    changes

--- src/energy.f90
+++ src/energy.f90
@@ -119,7 +119,7 @@ etot4    =  etot4 + sum(uext*den)*dxyz
 			 rXHe(2) = rimp(2) - y(iy)
 			 rXHe(3) = rimp(3) - z(iz)
 			 r = sqrt(sum(rXHe * rXHe))			 
-			 if (r >= 5.7) then
+			 if (r >= 6) then
 			 	eHeX  = eHeX  + den(ix,iy,iz) * uimp(ix,iy,iz)
 			 	uplus = uplus + den(ix,iy,iz) * Select_Pot(selec_plus,r,r_cutoff_plus,umax_plus)
 			 endif
