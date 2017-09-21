be7d789fdcc554119a99c1f18b9050e822f92fbe
François Coppens <francois.coppens@irsamc.ups-tlse.fr>
2017-09-21 18:04:29 +0200

    changes

--- get_impurity_energy.sh
+++ get_impurity_energy.sh
@@ -7,12 +7,10 @@ INPUT=${1}
 ln -sf ${INPUT} djogger.dat
 ./4hetddft-anisotropic < input.dat > res.dat
 TIME=$(head -n 3 djogger.dat | awk '/Total evolution time/{print$5}')
-XIMP=$(head djogger.dat | awk 'NR==9{print$1}')
-YIMP=$(head djogger.dat | awk 'NR==9{print$2}')
-ZIMP=$(head djogger.dat | awk 'NR==9{print$3}')
+RCM=$(awk '/X-HE CoM distance/{print$4}' res.dat)
 EKIN=$(awk '/Kinetic energy \(X/{print$5}' res.dat)
 EINT=$(awk '/Interaction energy \(X\*/{print$5}' res.dat)
 EINTPLUS=$(awk '/Interaction energy \(X\+/{print$5}' res.dat)
 ESO=$(awk '/Spin-Orbit energy/{print$5}' res.dat)
 rm djogger.dat res.dat DFT4He3d.namelist.read
-printf "%.1f\t\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n" ${TIME} ${XIMP} ${YIMP} ${ZIMP} ${EKIN} ${EINT} ${EINTPLUS} ${ESO}
+printf "%.1f\t\t%f\t%f\t%f\t%f\t%f\n" ${TIME} ${RCM} ${EKIN} ${EINT} ${EINTPLUS} ${ESO}
